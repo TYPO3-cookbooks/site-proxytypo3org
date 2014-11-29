3ba45618c46022679916071bd7d0ae277f15fe43
Steffen Gebert <steffen.gebert@typo3.org>
2014-11-29 23:54:47 +0100

    Only pin nginx

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name             'site-proxytypo3org'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
-license          'all_rights'
+maintainer       'Steffen Gebert'
+maintainer_email 'steffen.gebert@typo3.org'
+license          'Apache2'
 description      'Installs/Configures site-proxytypo3org'
 long_description 'Installs/Configures site-proxytypo3org'
 version          '0.1.0'
 
+depends "nginx", "= 1.6.0"
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,4 @@
 # Cookbook Name:: site-proxytypo3org
 # Recipe:: default
 #
-# Copyright (c) 2014 The Authors, All Rights Reserved.
+# Copyright (c) 2014 Steffen Gebert / TYPO3 Association
