7a89456cfd63c204b0acb138bd8bed7b709a1dc4
Steffen Gebert <steffen.gebert@typo3.org>
2016-01-23 17:34:50 +0100

    Compact listen declaration

--- recipes/_nginx_sites.rb
+++ recipes/_nginx_sites.rb
@@ -15,9 +15,7 @@ sites.each do |site|
 
   # HTTPS vhost
   nginx_conf_file site['name'] do
-    listen(
-      ["443 ssl http2", "[::]:443 ssl http2"]
-    )
+    listen(["443 ssl http2", "[::]:443 ssl http2"])
     socket site['nginx']['backend']
     # using our custom template, we don't have to supply certificate/key data here..
     ssl {}
@@ -28,9 +26,7 @@ sites.each do |site|
 
   # HTTP vhost, redirecting to HTTPS
   nginx_conf_file site['name'] + "-redirect" do
-    # listen(
-    #   ["80", "[::]:80"]
-    # )
+    listen ["80", "[::]:80"]
     server_name site['name']
     site_type :static
     options(
