f2e168ab10517e2d110fcf95c8f670ebbc400ce5
Steffen Gebert <steffen.gebert@typo3.org>
2016-01-23 13:54:24 +0100

    Configure nginx default site to redirect to typo3.org

--- recipes/_nginx_config.rb
+++ recipes/_nginx_config.rb
@@ -1,4 +1,24 @@
-# looks like node['nginx']['default_site_enabled'] does not match this
-file "/etc/nginx/conf.d/default.conf" do
-  action :delete
+########################
+# Default sites
+#
+# Set up defaults sites for port 80 and 443 that redirect to typo3.org.
+########################
+{
+  "http"  => "80",
+  "https" => "443 ssl http2"
+}.each do |protocol, listen|
+
+  nginx_conf_file "default-#{protocol}" do
+    listen(
+      ["#{listen} default", "[::]:#{listen} default"]
+    )
+    site_type :static
+    ssl {} if protocol == "https"
+    template "nginx_site.erb"
+    cookbook cookbook_name
+    options(
+      "return" => "301 https://typo3.org"
+    )
+  end
+
 end
