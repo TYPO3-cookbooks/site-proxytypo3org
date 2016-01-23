4b2d118447a47c1b78a766025bf8380dbc063917
Steffen Gebert <steffen.gebert@typo3.org>
2016-01-23 19:10:26 +0100

    Revert "Set header `HTTPS=on` so that backend knows it"
    This reverts commit 0016c96ea0704089fe4cbf8eb76735859825c963.

--- recipes/_nginx_sites.rb
+++ recipes/_nginx_sites.rb
@@ -9,10 +9,6 @@ sites = search(:proxy, "nginx:*")
 sites.each do |site|
   log "Processing nginx proxy site #{site}"
 
-  site_options = site['nginx']['options'] || {}
-  site_options["add_header"] = {} unless site_options["add_header"]
-  site_options["add_header"]["HTTPS"] = '"on"'
-
   # HTTPS vhost
   nginx_conf_file site['name'] do
     listen(["443 ssl http2", "[::]:443 ssl http2"])
@@ -21,13 +17,14 @@ sites.each do |site|
     ssl {}
     template "nginx_site.erb"
     cookbook cookbook_name
-    options site_options
+    options site['nginx']['options'] || {}
   end
 
   # HTTP vhost, redirecting to HTTPS
   nginx_conf_file site['name'] + "-redirect" do
     listen ["80", "[::]:80"]
     server_name site['name']
+    options site['nginx']['options'] || {}
     site_type :static
     options(
       "return" => "301 https://$server_name$request_uri"
