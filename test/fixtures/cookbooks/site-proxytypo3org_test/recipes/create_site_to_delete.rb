2ff502ff50ce360db31b75725ff94ae7ec302a99
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-18 16:15:37 +0100

    [BUGFIX] Ordering of test recipe

--- test/fixtures/cookbooks/site-proxytypo3org_test/recipes/create_site_to_delete.rb
+++ test/fixtures/cookbooks/site-proxytypo3org_test/recipes/create_site_to_delete.rb
@@ -1,9 +1,11 @@
 # test "action": "delete"
 # create a site that should be deleted during the regular run
 
-# this recipe has to run _before_ site-proxytypo3org::default
+# this recipe has to run _before_ site-proxytypo3org::_nginx_sites starts,
+# so we hook into a resource that runs bevore (in ::_nginx_config)
 
 nginx_conf_file "test.action.delete" do
   listen(["443 ssl http2", "[::]:443 ssl http2"])
   socket "http://example.org:80"
+  subscribes :create, "nginx_conf_file[default-http]", :before
 end
