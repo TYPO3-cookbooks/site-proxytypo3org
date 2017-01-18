43eb644b33701611338e2bf8faec5c9d23121dfd
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-18 15:57:02 +0100

    [BUGFIX] Specify dependency on nginx_conf cookbook in test cookbook

--- test/fixtures/cookbooks/site-proxytypo3org_test/metadata.rb
+++ test/fixtures/cookbooks/site-proxytypo3org_test/metadata.rb
@@ -4,3 +4,6 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Tests for gerrit"
 version          "0.0.0"
+
+# this should be kept in line with the version in the main cookbook
+depends          "nginx_conf", "= 1.0.1"
