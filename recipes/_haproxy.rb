b802447ca265feef47a6b303edba5bf572a5b46b
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-19 13:35:28 +0100

    [TASK] Update to haproxy cookbook 2.0.2
    Changelog says that it now tests config first prior to write
    invalid config.

--- recipes/_haproxy.rb
+++ recipes/_haproxy.rb
@@ -10,6 +10,6 @@ include_recipe "#{cookbook_name}::_haproxy_sites"
 # create the directory, where we want our stats socket
 directory "/var/run/haproxy"
 
-include_recipe "haproxy"
+include_recipe "haproxy::manual"
 
 include_recipe "zabbix-custom-checks::haproxy" unless node['dev_mode']
